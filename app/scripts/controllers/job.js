23d746ef8da584cfa94abc99197e1c24e2e63b82
Kyle Derkacz <kyleder@gmail.com>
2014-05-19 10:08:12 -0700

    Adding additional data points and the beginning of filtering (not working)

--- app/scripts/controllers/job.js
+++ app/scripts/controllers/job.js
@@ -5,7 +5,7 @@ angular.module('reclusedashApp')
 
         $scope.totalUrlCount = 0;
         $scope.currentPage = 1;
-        $scope.itemsPerPage = 20;
+        $scope.itemsPerPage = 15;
         $scope.filters = [];
 
         CrawlUrl.jobs($routeParams.jobId)
@@ -22,6 +22,7 @@ angular.module('reclusedashApp')
                 /*jshint camelcase: false */
                 $scope.urls = data.objects || [];
                 $scope.totalUrlCount = data.meta.total_count;
+                console.log(data.meta.total_count);
                 console.log(data);
                 /*jshint camelcase: true */
             })
@@ -107,6 +108,78 @@ angular.module('reclusedashApp')
             I21: 'One or more links on the page has robots = noindex',
         };
 
+        $scope.httpCodes = {
+            100: '100: Continue',
+            101: '101: Switching Protocols',
+            102: '102: Processing',
+            200: '200: OK',
+            201: '201: Created',
+            202: '202: Accepted',
+            203: '203: Non-Authoritative Information',
+            204: '204: No Content',
+            205: '205: Reset Content',
+            206: '206: Partial Content',
+            207: '207: Multi-Status',
+            208: '208: Already Reported',
+            226: '226: IM Used',
+            300: '300: Multiple Choices',
+            301: '301: Moved Permanently',
+            302: '302: Found',
+            303: '303: See Other',
+            304: '304: Not Modified',
+            305: '305: Use Proxy',
+            306: '306: (Unused)',
+            307: '307: Temporary Redirect',
+            308: '308: Permanent Redirect',
+            400: '400: Bad Request',
+            401: '401: Unauthorized',
+            402: '402: Payment Required',
+            403: '403: Forbidden',
+            404: '404: Not Found',
+            405: '405: Method Not Allowed',
+            406: '406: Not Acceptable',
+            407: '407: Proxy Authentication Required',
+            408: '408: Request Timeout',
+            409: '409: Conflict',
+            410: '410: Gone',
+            411: '411: Length Required',
+            412: '412: Precondition Failed',
+            413: '413: Payload Too Large',
+            414: '414: URI Too Long',
+            415: '415: Unsupported Media Type',
+            416: '416: Requested Range Not Satisfiable',
+            417: '417: Expectation Failed',
+            422: '422: Unprocessable Entity',
+            423: '423: Locked',
+            424: '424: Failed Dependency',
+            425: '425: Unassigned',
+            426: '426: Upgrade Required',
+            427: '427: Unassigned',
+            428: '428: Precondition Required',
+            429: '429: Too Many Requests',
+            430: '430: Unassigned',
+            431: '431: Request Header Fields Too Large',
+            500: '500: Internal Server Error',
+            501: '501: Not Implemented',
+            502: '502: Bad Gateway',
+            503: '503: Service Unavailable',
+            504: '504: Gateway Timeout',
+            505: '505: HTTP Version Not Supported',
+            506: '506: Variant Also Negotiates (Experimental)',
+            507: '507: Insufficient Storage',
+            508: '508: Loop Detected',
+            509: '509: Unassigned',
+            510: '510: Not Extended',
+            511: '511: Network Authentication Required'
+        };
+
+        $scope.contentTypes = {
+            'text/html': 'HTML',
+            'text/xml': 'XML',
+            'image/jpeg': 'Image (JPG)',
+            'image/png': 'Image (PNG)',
+        };
+
         // CrawlUrl.job($routeParams.jobId, 0, 1)
         // .success(function (data) {
         //     /*jshint camelcase: false */
@@ -127,9 +200,11 @@ angular.module('reclusedashApp')
             else {
                 $scope.activeUrl = url;
                 // Load the inbound and outbound links on the page
+                /*jshint camelcase: false */
                 CrawlLink.get({to_url: url.id}, function (res) {
                     url.toLinks = res.objects;
                 });
+                /*jshint camelcase: true */
             }
         };
 
---
+++ app/scripts/filters/truncate.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('reclusedashApp')
+    .filter('truncate', function() {
+        return function(text, length, end) {
+            if (isNaN(length)) {
+                length = 10;
+            }
+
+            if (end === undefined) {
+                end = '...';
+            }
+
+            if (text.length <= length || text.length - end.length <= length) {
+                return text;
+            }
+            else {
+                return String(text).substring(0, length-end.length) + end;
+            }
+
+        };
+    });
